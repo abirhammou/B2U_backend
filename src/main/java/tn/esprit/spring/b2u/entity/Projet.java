c707ca9cd6b1e318875c2914d34d855d943061e8
mariem <mariem.benfradj19@gmail.com>
2026-04-07 00:13:54 +0200

    clean repo: remove .idea and target

--- src/main/java/tn/esprit/spring/b2u/entity/Projet.java
+++ src/main/java/tn/esprit/spring/b2u/entity/Projet.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+import java.util.List;
+
 @Document(collection = "projets")
 @Data
 @AllArgsConstructor
@@ -15,9 +18,14 @@ public class Projet {
     @Id
     private String id;
 
-    private String nomProjet;
-    private String descriptionProjet;
-    private String delaiEstime;
-    private Double prixProjet;
-    private String idEquipeAssocie;
+    private String title;
+    private String description;
+    private String companyId;
+    private String companyName;
+    private List<String> requiredSkills;
+    private int teamSize;
+    private Date deadline;
+    private String status;
+    private int applicantsCount;
+    private Date createdAt;
 }
