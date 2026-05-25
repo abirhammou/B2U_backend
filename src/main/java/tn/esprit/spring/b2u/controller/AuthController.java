184631427e8044d9e9ef1b0d3a8d5e53b271eede
Abir Hammou <abirhammou2001@gmail.com>
2026-05-25 13:13:18 +0200

    Save local changes before merging main

--- src/main/java/tn/esprit/spring/b2u/controller/AuthController.java
+++ src/main/java/tn/esprit/spring/b2u/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
